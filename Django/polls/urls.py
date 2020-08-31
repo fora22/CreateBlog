1848f962077864b46c295da19a6ce1014321df0e
팽대원 <fora22@kyonggi.ac.kr>
2020-08-31 22:34:19 +0900

    기능 : 예제 파일 작성 완료
    이상하게 question에서 add 해서 save 하려고 하면 에러 뜸
    학교 서버라서 그럴 수도 있음
    확인 필요

--- Django/polls/urls.py
+++ Django/polls/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from polls import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')), # 추가
+    path('', views.index, name='index'),    # /polls/
+    path('<int:question_id>/', views.detail, name='detail'),        # /polls/5/
+    path('<int:question_id>/results/', views.results, name='results'),      # /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),       # /polls/5/vote/
 ]
