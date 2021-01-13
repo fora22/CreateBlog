516cfe108930a22fddc9ad039fe527967bafc2af
팽대원 <fora22@kyonggi.ac.kr>
2021-01-13 16:41:45 +0900

    main blog 다 만듬

--- Develope/fora22/VanillaJS/main.js
+++ Develope/fora22/VanillaJS/main.js
@@ -1,13 +1,37 @@
-// import  "./styles"
 const documentBody = document.querySelector("body");
 const docHeading = document.createElement('h1');
 const post = [];
+const postLink = [
+    './post/BinarySearch.html',
+    './post/Stack.html'
+]
+const postTitle = [
+    '이진 탐색',
+    '스택 구조'
+]
+
+function postListDesign(index) {
+    const postHtml = document.createElement('a');
+    postHtml.href = postLink[index];
+    postHtml.title = postTitle[index];
+    postHtml.innerText = postTitle[index];
+
+    const postDate = document.createElement('div');
+    postDate.innerText = new Date();
+    postHtml.appendChild(postDate);
+
+    return postHtml;
+}
+
 function makePostList(pList) {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < postLink.length; i++) {
         post.push(function() {
             const tempPost = document.createElement('div');
             tempPost.className = "postItem"
-            tempPost.innerText = `${i}`;
+            
+            const postItem = postListDesign(i);
+            tempPost.appendChild(postItem);
+
             return tempPost;
         }());
         pList.appendChild(post[i]);
